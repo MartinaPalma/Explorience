9cd5d5d99c1775febb4bcc580a2cb9212e886a20
Casimiro <cp.ciancimino@gmail.com>
2021-04-15 00:11:58 +0200

    feature: add routing card activity detail from homepage

--- components/cardactivities/index.jsx
+++ components/cardactivities/index.jsx
@@ -15,17 +15,20 @@ const CardActivities = () => {
     <section className={styles.CardActivities}>
       <h3>Scopri le attività più ricercate</h3>
       <h4>Lasciati trasportare dalle attività più ambite</h4>
+
       <div className={styles.scroll}>
         {activities
           .filter((activity) => activity.cover_image_url)
           .map((activity) => {
-            const { cover_image_url, uuid } = activity
             return (
               <CardActivity
-                image={cover_image_url}
+                allData={activity}
+                city={activity.city}
+                uuid={activity.uuid}
+                image={activity.cover_image_url}
                 code={activity.verticals[0].code}
                 relevance={activity.verticals[0].relevance}
-                key={uuid}
+                key={activity.uuid}
               />
             )
           })}
--- components/cardactivity/index.jsx
+++ components/cardactivity/index.jsx
@@ -1,10 +1,27 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 import styles from './index.module.scss'
 
-function CardActivity({ image, code, relevance }) {
+const getActivityUrlTitle = (fullURL) => {
+  const splittedURL = fullURL.split('/')
+  return splittedURL[splittedURL.length - 2]
+}
+
+function CardActivity({ image, code, relevance, uuid, city, allData }) {
+  const router = useRouter()
+
   return (
     <article className={styles.CardActivity}>
       <h5 className={styles.relevance}>{code}</h5>
-      <img src={image} alt={code} />
+      <Link
+        href={{
+          pathname: `/activity/${getActivityUrlTitle(city.url)}`,
+          query: { uuid: uuid },
+        }}
+        as={`/activity/${getActivityUrlTitle(city.url)}`}
+      >
+        <img src={image} alt={code} />
+      </Link>
       <p style={{ color: '#171717' }}>{relevance / 10 / 2}</p>
     </article>
   )
