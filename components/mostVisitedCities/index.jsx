e698f229a379d96b774c897c4f2fd2535fc2928e
Martina <martina@palma>
2021-04-14 20:10:47 +0200

    feature: cities populated in cards homepage

--- components/mostVisitedCities/index.jsx
+++ components/mostVisitedCities/index.jsx
@@ -1,17 +1,31 @@
 import CardCities from '../CardCities'
-import { data } from '../data'
 import styles from './index.module.scss'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const MostVisitedCities = () => {
+  const [cities, setCities] = useState([])
+  useEffect(() => {
+    axios.get('https://sandbox.musement.com/api/v3/cities').then((res) => {
+      const { data } = res
+      setCities(data)
+    })
+  }, [])
   return (
     <section className={styles.MostVisitedCities}>
       <h3>Scopri le città più visitate</h3>
       <h4>Lasciati ispirare dalle mete più ambite</h4>
       <div className={styles.scroll}>
-        {/* Scroll temporaneo */}
-        {data.map((city) => {
-          const { image, title, mark, id } = city
-          return <CardCities image={image} title={title} mark={mark} key={id} />
+        {cities.map((city) => {
+          const { cover_image_url, activities_count, name, uuid } = city
+          return (
+            <CardCities
+              image={cover_image_url}
+              name={name}
+              activities_count={activities_count}
+              key={uuid}
+            />
+          )
         })}
       </div>
     </section>
