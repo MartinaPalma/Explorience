da72e33e52a2e8fdf990812ea622794ba58d7527
MartinaPalma <78905688+MartinaPalma@users.noreply.github.com>
2021-04-29 11:45:24 +0200

    refactor: header home (#82)
    * refactor: header home
    
    * refactor: header home
    
    Co-authored-by: Martina <martina@palma>
    Co-authored-by: casiimir <cp.ciancimino@gmail.com>

--- components/headerHome/index.jsx
+++ components/headerHome/index.jsx
@@ -4,17 +4,26 @@ import { images } from '../../utils/images'
 const HeaderHome = () => {
   return (
     <header className={styles.HeaderHome}>
-      <div
-        className={styles.image}
-        style={{
-          backgroundImage: `url("${images[3].url}")`,
-        }}
-      ></div>
-      <div className={styles.title}>
-        <h1>explorience</h1>
-        <h2>
-          Explore, Experience, <em style={{ color: '#3c9ace' }}>Enjoy</em>
+      <div className={styles.HeaderHome_image}></div>
+
+      <div className={styles.HeaderHome_titleWrapper}>
+        <h1 className={styles.HeaderHome_title}>explorience</h1>
+        <h2 className={styles.HeaderHome_subtitle}>
+          Explore, Experience, <em>Enjoy</em>
         </h2>
+      </div>
+
+      <div className={styles.HeaderHome_cityWrapper}>
+        <h3 className={styles.HeaderHome_city}>Dubai</h3>
+        <p className={styles.HeaderHome_aphorisms}>
+          Le persone non fanno i viaggi, sono i viaggi che fanno le persone
+        </p>
+        <a
+          href="https://explorience.vercel.app/city/Dubai"
+          className={styles.HeaderHome_button}
+        >
+          Visita
+        </a>
       </div>
     </header>
   )
